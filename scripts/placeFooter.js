3490be6cef534d54ff35f44c649c671ffc3d75b2
Wang Jian <337034664@qq.com>
2017-07-24 15:06:41 +0800

    在底部固定footer

---
+++ scripts/placeFooter.js
@@ -0,0 +1,14 @@
+function placeFooter() {
+    var footerHeight = $("footer").outerHeight();
+
+    if ($(document).height() + footerHeight != $(window).height()) {
+        $("footer").removeClass("navbar-fixed-bottom");
+    }
+
+    if ($(document).height() == $(window).height()) {
+        $("footer").addClass("navbar-fixed-bottom");
+    }
+}
+
+$(document).ready(placeFooter);
+window.onresize = placeFooter;
