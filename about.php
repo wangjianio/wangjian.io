8beb0fe6c22dba61c1fbe0264b3c95da46567b51
Wang Jian <337034664@qq.com>
2017-07-27 00:01:04 +0800

    增加感谢列表

--- about.php
+++ about.php
@@ -2,14 +2,36 @@
 $title = '关于';
 $nav_type = 'about';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
-
-echo <<<H1
+?>
   <div class="container">
     <div class="page-header">
-      <h1>$title</h1>
+      <h1><?php echo $title; ?></h1>
     </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-md-8">
+        <p>aaa</p>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4">
+        <h2>特别感谢</h2>
+        <ul class="list-unstyled">
+          <li><a href="http://httpd.apache.org" target="_blank">Apache HTTP Server Project</a></li>
+          <li><a href="http://getbootstrap.com" target="_blank">Bootstrap</a></li>
+          <li><a href="https://clipboardjs.com" target="_blank">clipboard.js</a></li>
+          <li><a href="https://getcomposer.org" target="_blank">Composer</a></li>
+          <li><a href="http://xdsoft.net/jqplugins/datetimepicker/" target="_blank">DateTimePicker</a></li>
+          <li><a href="https://github.com" target="_blank">GitHub</a></li>
+          <li><a href="http://glyphicons.com" target="_blank">GLYPHICONS</a></li>
+          <li><a href="https://www.javascript.com" target="_blank">JavaScript</a></li>
+          <li><a href="https://jquery.com" target="_blank">jQuery</a></li>
+          <li><a href="http://parsedown.org" target="_blank">Parsedown</a></li>
+          <li><a href="http://php.net" target="_blank">PHP</a></li>
+          <li><a href="https://www.w3.org" target="_blank">W3C</a></li>
+        </ul>
+      </div>
+    </div>
+    
+
+
   </div><!-- .container -->
-H1;
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';
-
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';
