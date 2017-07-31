0e55aad346960d6f601c45ea4f499a5eb7d1dffc
Wang Jian <337034664@qq.com>
2017-07-31 10:29:50 +0800

    优化日期显示代码

--- projects/money/index.php
+++ projects/money/index.php
@@ -1,7 +1,7 @@
 <?php
 namespace lopedever\money;
 
-include __DIR__ . '/includes/database/database.php';
+require_once __DIR__ . '/includes/database.php';
 include __DIR__ . '/includes/index/PrintIndexTransData.php';
 include __DIR__ . '/includes/index/PrintAddTransForm.php';
 
@@ -16,76 +16,12 @@ $extra_js .= '<script src="scripts/money.js"></script>';
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 
+$arr_month = array("无","一","二","三","四","五","六","七","八","九","十","十一","十二");
+$arr_week = array("日","一","二","三","四","五","六");
+
 $year = date('Y');
-$month = date('n');
-switch ($month) {
-  case '1':
-    $month = '一月';
-    break;
-  case '2':
-    $month = '二月';
-    break;
-  case '3':
-    $month = '三月';
-    break;
-  case '4':
-    $month = '四月';
-    break;
-  case '5':
-    $month = '五月';
-    break;
-  case '6':
-    $month = '六月';
-    break;
-  case '7':
-    $month = '七月';
-    break;
-  case '8':
-    $month = '八月';
-    break;
-  case '9':
-    $month = '九月';
-    break;
-  case '10':
-    $month = '十月';
-    break;
-  case '11':
-    $month = '十一月';
-    break;
-  case '12':
-    $month = '十二月';
-    break;
-
-  default:
-    break;
-}
-$week = date('N');
-switch ($week) {
-  case '1':
-    $week = '星期一';
-    break;
-  case '2':
-    $week = '星期二';
-    break;
-  case '3':
-    $week = '星期三';
-    break;
-  case '4':
-    $week = '星期四';
-    break;
-  case '5':
-    $week = '星期五';
-    break;
-  case '6':
-    $week = '星期六';
-    break;
-  case '7':
-    $week = '星期日';
-    break;
-
-  default:
-    break;
-}
+$month = $arr_month[date('n')];
+$week = $arr_week[date('N')];
 $day = date('d');
 ?>
   <div class="container">
@@ -94,8 +30,8 @@ $day = date('d');
         <div class="row">
           <div class="col-xs-12 h1" style="width:320px;">
                 <span class="pull-left day" style="font-size: 80px; margin-right: 8px;"><?php echo $day; ?></span>
-                <span class="pull-left week" style="font-size: 24px; margin-top: 16px;"><?php echo $week; ?></span>
-                <span class="pull-left month" style="font-size: 32px;"><?php echo "$month $year"; ?></span>
+                <span class="pull-left week" style="font-size: 24px; margin-top: 16px;"><?php echo "星期$week"; ?></span>
+                <span class="pull-left month" style="font-size: 32px;"><?php echo "{$month}月 $year"; ?></span>
           </div>
           <a class="pull-right hidden-xs hidden-sm" id="addLink" href data-toggle="modal" data-target="#addTransFormModal" style="position: absolute; right: 15px; top: 20px">
             <span class="glyphicon glyphicon-plus-sign" aria-hidden="true" style="font-size: 80px"></span>
