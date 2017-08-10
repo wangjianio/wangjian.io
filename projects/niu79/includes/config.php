711438ef69d190565d4ad6686993af8e9cdecbf0
Wang Jian <337034664@qq.com>
2017-08-10 13:34:27 +0800

    更新

--- projects/niu79/includes/config.php
+++ projects/niu79/includes/config.php
@@ -8,6 +8,6 @@ define('USERNAME', 'niujiao');
 define('PASSWORD', 'xiaobo');
 
 define('TITLE', '彩虹卡');
-define('SHOW', false);
+define('SHOW', true);
 define('TIP', '1.长按可保存彩虹卡为图片；
 2.彩虹卡每日更新，每日一张。');
