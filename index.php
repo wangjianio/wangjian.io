17b93237c0559eeacb47e33b8bca94d3bf64bcaf
Wang Jian <337034664@qq.com>
2017-08-10 13:49:35 +0800

    更新主页

--- index.php
+++ index.php
@@ -6,8 +6,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
     <div class="jumbotron">
       <div class="container" style="max-width: 970px">
         <h1>主页</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes
-          the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>欢迎访问</p>
         <p>To see the difference between static and fixed top navbars, just scroll.</p>
         <p>
           <a class="btn btn-lg btn-primary" href="/" role="button">View navbar docs &raquo;</a>
