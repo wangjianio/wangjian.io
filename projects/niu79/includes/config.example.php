7dfcd881fc1b0c4ecd15bb326b0c2461c200447a
Wang Jian <337034664@qq.com>
2017-08-10 13:42:17 +0800

    添加 示例配置文件

---
+++ projects/niu79/includes/config.example.php
@@ -0,0 +1,12 @@
+<?php
+namespace wangjian\wangjianio\projects\niu79\bookmark;
+
+date_default_timezone_set('Asia/Shanghai');
+
+// 登录账户密码
+define('USERNAME', 'username');
+define('PASSWORD', 'password');
+
+define('TITLE', 'title');
+define('SHOW', true);
+define('TIP', 'tip');
