f16dc156d6b7667ef4cd368250d1677e2cbf3fb3
Wang Jian <337034664@qq.com>
2017-08-15 12:51:24 +0800

    关闭错误显示

--- projects/money/includes/Database.php
+++ projects/money/includes/Database.php
@@ -45,9 +45,6 @@ class Database
                 break;
         }
 
-        ini_set('display_errors', true);
-        error_reporting(E_ALL);
-
         $this->mysqli = new \mysqli($hostname, $username, $password, $database);
 
         // 如果连接错误输出错误信息，否则将编码设置为 UTF-8
