bb1a1c57f3785387c9be4701dcdf1155ec6c6a50
Wang Jian <337034664@qq.com>
2017-08-16 19:21:24 +0800

    改进
    提高性能，增加排序，简化代码

--- projects/niu79/bookmark/includes/functions.php
+++ projects/niu79/bookmark/includes/functions.php
@@ -5,16 +5,21 @@ class Index
 {
     public function showFileName($dir)
     {
-        if ($handle = opendir($dir)) {
-            while (false !== ($file_name = readdir($handle))) {
-                if ($file_name != "." && $file_name != "..") {
-                    $file_src = "../images/$file_name";
-                    echo "<tr><td id='file-name'>$file_name</td><td><a href='$file_src' target='_blank'>查看</a></td><td><a href='server?action=delete&file_name=$file_name'>删除</a></td></tr>\n";
-                    //<img src='$file_src'>
-                }
+        $file_list = scandir($dir, 1);
+        
+        foreach ($file_list as &$file_name) {
+            if ($file_name != '.' && $file_name != '..') {
+                $file_path = "../images/$file_name";
+                echo <<<TBODY
+                <tr>
+                    <td>$file_name</td>
+                    <td><a href="$file_path" target="_blank">查看</a></td>
+                    <td><a href="server?action=delete&file_name=$file_name">删除</a></td>
+                </tr>
+TBODY;
             }
-        closedir($handle);
         }
+        unset($file_name);
     }
 }
 
