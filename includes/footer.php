7bc673bf3598b5116961bd712b9cc1e4831f20e1
Wang Jian <337034664@qq.com>
2017-08-17 15:34:21 +0800

    更改 footer 内容

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,18 @@
     <footer>
       <div class="container">
         <div class="row">
-          <div class="col-sm-8">
+          <div class="col-xs-12 col-sm-6 col-md-8">
             <p class="h5">联系方式</p>
             <ul class="list-unstyled">
-              <li><a href="mailto:admin@wangjian.blog"><img class="svg" alt="mail-icon" src="/icons/mail.svg">contact@wangjian.io</a></li>
+              <li><a href="mailto:contact@wangjian.io"><img class="svg" alt="mail-icon" src="/icons/mail.svg">contact@wangjian.io</a></li>
               <li><a href="https://twitter.com/lopedever" target="_blank"><img class="svg" alt="twitter-icon" src="/icons/twitter-icon.svg">@lopedever</a></li>
-              <li><a href="https://github.com/wangjianio" target="_blank"><img class="svg" alt="github-icon" src="/icons/github-icon.svg">@wangjianio</a></li>
+              <!-- <li><a href="https://github.com/wangjianio" target="_blank"><img class="svg" alt="github-icon" src="/icons/github-icon.svg">@wangjianio</a></li> -->
             </ul>
           </div>
-          <div class="col-sm-4">
-            <p class="h3">wangjian.io</p>
-            <p>Made by Wang Jian,<br>and open sourced on <a href="https://github.com/wangjianio/wangjian.io" target="_blank">Github</a>.</p>
-            <p><a href="http://www.miibeian.gov.cn" target="_blank">蒙ICP备17002783号</a></p>
+          <div class="col-xs-12 col-sm-6 col-md-4">
+            <p class="h4">wangjian.io</p>
+            <p class="small">Made by Wang Jian, and open sourced on <a href="https://github.com/wangjianio/wangjian.io" target="_blank">Github</a>.</p>
+            <p class="small"><a class="text-muted" href="http://www.miibeian.gov.cn" target="_blank">蒙ICP备17002783号</a></p>
           </div>
         </div>
       </div>
