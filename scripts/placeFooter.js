60cb44ef714ffe3c643100832dd9bb19a150c8ae
Wang Jian <337034664@qq.com>
2017-08-25 01:01:12 +0800

    增加 MutationObserver

--- scripts/placeFooter.js
+++ scripts/placeFooter.js
@@ -11,5 +11,21 @@ function placeFooter() {
 }
 
 placeFooter();
-$(document).ready(placeFooter);
-window.onresize = placeFooter;
+$(placeFooter);
+$(window).on('resize', placeFooter)
+
+// MutationObserver 兼容性测试。因不兼容影响也不大，所以此处只做记录，未做处理。
+// var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+// var mutationObserverSupport = !!MutationObserver;
+
+var mo = new MutationObserver(function () {
+   placeFooter(); 
+});
+
+var option = {
+    'childList': true, 
+    'subtree': true,
+    // 'attributeFilter': ['height'],
+};
+
+mo.observe(document.body, option);
