c5457ec2fd33417b87002e1356d9167acafae1d2
Wang Jian <337034664@qq.com>
2017-08-25 23:52:47 +0800

    使用 sticky 解决

--- projects/money/category/index.php
+++ projects/money/category/index.php
@@ -8,7 +8,8 @@ $nav_type = 'money';
 $subnav_type = 'category';
 
 $extra_css  = '<link rel="stylesheet" href="/node_modules/bootstrap-treeview/src/css/bootstrap-treeview.css">';
-$extra_css  = '<style>h1 a, h1 a:hover { color: #333; }</style>';
+$extra_css .= '<link rel="stylesheet" href="sticky.css">';
+$extra_css .= '<style>h1 a, h1 a:hover { color: #333; }</style>';
 $extra_js   = '<script src="/node_modules/bootstrap-treeview/src/js/bootstrap-treeview.js"></script>';
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
@@ -54,7 +55,7 @@ PAGE_HEADER;
           <div id="tree"></div>
         </div><!-- col -->
 
-        <div class="col-sm-5" id="right_col">
+        <div class="col-sm-5 sticky" id="right_col">
           <div class="panel panel-default">
             <div class="panel-body">
               <div class="form-group">
@@ -65,15 +66,15 @@ PAGE_HEADER;
               <button class="btn btn-success btn-block" id="submit_btn" type="button">提交</button>
             </div>
           </div>
-          <button class="btn btn-default btn-block" id="test_btn" type="button">测试</button>
         </div>
       </div><!-- row -->
-
+      
     </div><!-- .container -->
-
+    
+    <!-- <p id="test" style="position: fixed; top: 100px; left:20px;">placeholder</p> -->
 
     <script>
-        $(document).ready(function () {
+        $(function () {
           $.ajax({
             type: "GET",
             url: "tree_view_json?type=<?php echo $type; ?>",
@@ -119,54 +120,8 @@ PAGE_HEADER;
             });
           })
 
-          $(window).resize(function () {
-            $('#test_btn').text($(window).height());
-            });
-            
-        $(window).resize(function () {
-          
-          if ($(window).height() >= 460) {
-            placeRightCol();
+      }); // ready
 
-            $(document).scroll(function () {
-              var win_width = $(window).width();
-              var bootstrap_sm_width = 768;
-              
-              if (win_width >= bootstrap_sm_width) {
-                placeRightCol();
-              }
-            });
-            
-            // $(window).resize(function() {
-            //   var win_height = $(window).height();
-            //   var win_width = $(window).width();
-            //   var bootstrap_sm_width = 768;
-  
-            //   if (win_width >= bootstrap_sm_width) {
-            //     placeRightCol();
-            //   } else {
-            //     $('#right_col').css('top', 'auto');
-            //   }
-            // });
-
-          } else {
-            $('#right_col').css('top', 'auto');
-          }
-
-        });
-
-          function placeRightCol() {
-            var item_top_to_window = document.getElementById('left_col').getBoundingClientRect().top;
-            var extra_top = 20 - item_top_to_window;
-
-            if (item_top_to_window < 20) {
-              $('#right_col').css('top', extra_top);
-            } else if (item_top_to_window > 20) {
-              $('#right_col').css('top', 'auto');
-            }
-          }
-          
-        });
     </script>
 
 <?php
