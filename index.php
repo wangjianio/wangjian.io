6454c7f2aa8bd3e971d1e010e96375d00299e0d4
Wang Jian <337034664@qq.com>
2017-09-08 17:14:58 +0800

    增加引导

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
     <div class="jumbotron">
       <div class="container" style="max-width: 970px">
-        <h1>主页</h1>
-        <p>欢迎访问</p>
+        <h1>欢迎访问</h1>
+        <p>最近在忙毕业设计，可以 <a href="/projects/money/">看看进度</a> 或者 <a href="https://github.com/wangjianio/wangjian.io/tree/master/projects/money" target="_blank">给我的代码提提意见</a>。</p>
       </div>
     </div>
     
