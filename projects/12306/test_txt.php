f2872ab3e00ec140e8c9ef2e037ebb2b6c805bdf
Wang Jian <337034664@qq.com>
2017-10-23 00:35:27 +0800

    增加测试文档

---
+++ projects/12306/test_txt.php
@@ -0,0 +1,13 @@
+<?php
+$title = '12306';
+$nav_type = 'tl12306';
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
+?>
+    <div class="container">
+      <div class="page-header">
+        <h1>12306 测试短信</h1>
+      </div>
+      <p>订单E115326057,您已购9月3日D3208次5车2D号余姚北15:55开,检票口A1。【铁路客服】</p>
+    </div>
+  </body>
+</html>
