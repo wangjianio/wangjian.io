20858a9b58644efab9b4140cd0ad93cb16d5a80c
Wang Jian <337034664@qq.com>
2017-10-23 00:16:33 +0800

    完善12306查询功能

--- projects/12306/index.php
+++ projects/12306/index.php
@@ -3,11 +3,34 @@ $title = '12306';
 $nav_type = 'tl12306';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
-<div class="container">
-<form method="get" action="get_station_telecode.php">
-    <input name="station_name" type="text">
-    <input type="submit">
-</form>
-</div>
-</body>
+    <div class="container">
+      <div class="page-header">
+        <h1>12306 信息</h1>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          <form method="get" action="server">
+            <div class="form-group">
+              <label for="train_code">车次：</label>
+              <input class="form-control" id="train_code" name="train_code" type="text" placeholder="D10">
+            </div>
+            <div class="form-group">
+              <label for="from_station_name">出发站：</label>
+              <input class="form-control" id="from_station_name" name="from_station_name" type="text" placeholder="北京">
+            </div>
+            <div class="form-group">
+              <label for="to_station_name">到达站：</label>
+              <input class="form-control" id="to_station_name" name="to_station_name" type="text" placeholder="上海">
+            </div>
+            <div class="form-group">
+              <label for="train_date">出发日期：</label>
+              <input class="form-control" id="train_date" name="train_date" type="date" placeholder="2017-10-10">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">提交</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
