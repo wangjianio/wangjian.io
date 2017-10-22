d440a89e9435176653e2479792cee056326fa623
Wang Jian <337034664@qq.com>
2017-10-23 01:13:42 +0800

    新增测试例子

--- projects/12306/test_txt.php
+++ projects/12306/test_txt.php
@@ -8,6 +8,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
         <h1>12306 测试短信</h1>
       </div>
       <p>订单E115326057,您已购9月3日D3208次5车2D号余姚北15:55开,检票口A1。【铁路客服】</p>
+      <p>订单E115326057,您已购11月1日G1次5车2D号北京南9:00开,检票口A1。【铁路客服】</p>
     </div>
   </body>
 </html>
