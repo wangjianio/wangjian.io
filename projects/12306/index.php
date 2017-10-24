ba53708b9b82b10856a8067ed78f712167547b79
Wang Jian <337034664@qq.com>
2017-10-24 23:42:28 +0800

    优化版式

--- projects/12306/index.php
+++ projects/12306/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = '12306';
+$title = '12306 信息查询';
 $nav_type = 'tl12306';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 
@@ -7,32 +7,44 @@ $current_date = date('Y-m-d');
 ?>
     <div class="container">
       <div class="page-header">
-        <h1>12306 信息</h1>
+        <h1><?php echo $title; ?></h1>
       </div>
-      <div class="row">
-        <div class="col-sm-6">
-          <form method="get" action="server">
-            <div class="form-group">
-              <label for="train_code">车次：</label>
-              <input class="form-control" id="train_code" name="train_code" type="text" placeholder="G1">
-            </div>
-            <div class="form-group">
+
+      <form method="get" action="server">
+
+        <div class="row">
+          <div class="col-sm-6" style="padding: 0">
+            <div class="form-group col-sm-6">
               <label for="from_station_name">出发站：</label>
               <input class="form-control" id="from_station_name" name="from_station_name" type="text" placeholder="北京南">
             </div>
-            <div class="form-group">
+            <div class="form-group col-sm-6">
               <label for="to_station_name">到达站：</label>
               <input class="form-control" id="to_station_name" name="to_station_name" type="text" placeholder="上海虹桥">
             </div>
-            <div class="form-group">
+            <div class="form-group col-sm-6">
+              <label for="train_code">车次：</label>
+              <input class="form-control" id="train_code" name="train_code" type="text" placeholder="G1">
+            </div>
+            <div class="form-group col-sm-6">
               <label for="train_date">出发日期：</label>
               <input class="form-control" id="train_date" name="train_date" type="date" placeholder="<?php echo $current_date; ?>" value="<?php echo $current_date; ?>">
             </div>
-            
-            <button type="submit" class="btn btn-primary">提交</button>
-          </form>
+            <div class="col-sm-12" style="margin: 5px 0 15px 0">
+              <button class="btn btn-primary btn-block" type="button">查询</button>
+            </div>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                Basic panel example
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+
+      </form>
+    </div><!-- container -->
   </body>
 </html>
