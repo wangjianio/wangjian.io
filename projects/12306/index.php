9ade9cff5f2f377d12031f0ea12efc9c0aa617ef
Wang Jian <337034664@qq.com>
2017-11-04 18:58:39 +0800

    更新

--- projects/12306/index.php
+++ projects/12306/index.php
@@ -3,7 +3,14 @@ $title = '12306 信息查询';
 $nav_type = 'tl12306';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 
-$current_date = date('Y-m-d');
+$current_hour = date('H');
+
+if ($current_hour < 12) {
+  $train_date_default = date('Y-m-d');
+} else {
+  $train_date_default = date('Y-m-d', strtotime("+1 day"));
+}
+
 ?>
     <div class="container">
       <div class="page-header">
@@ -28,17 +35,16 @@ $current_date = date('Y-m-d');
             </div>
             <div class="form-group col-sm-6">
               <label for="train_date">出发日期：</label>
-              <input class="form-control" id="train_date" name="train_date" type="date" placeholder="<?php echo $current_date; ?>" value="<?php echo $current_date; ?>">
+              <input class="form-control" id="train_date" name="train_date" type="date" placeholder="<?php echo $train_date_default; ?>" value="<?php echo $train_date_default; ?>" autocomplete="off">
             </div>
             <div class="col-sm-12" style="margin: 5px 0 15px 0">
-              <button class="btn btn-primary btn-block" type="button">查询</button>
+              <button class="btn btn-primary btn-block" id="btn-submit" type="button">查询</button>
             </div>
           </div>
 
-          <div class="col-sm-6">
+          <div class="col-sm-6" id="result-panel" style="display: none">
             <div class="panel panel-default">
-              <div class="panel-body">
-                Basic panel example
+              <div class="panel-body" id="result-panel-body">
               </div>
             </div>
           </div>
@@ -46,5 +52,42 @@ $current_date = date('Y-m-d');
 
       </form>
     </div><!-- container -->
+    <script>
+      
+      $(function () {
+        
+        $('#btn-submit').on('click', function () {
+          var from_station_name = $('#from_station_name').val();
+          var to_station_name = $('#to_station_name').val();
+          var train_code = $('#train_code').val();
+          var train_date = $('#train_date').val();
+          $.ajax({
+            type: 'get',
+            url: 'server',
+            dataType: 'json',
+            data: {
+              'action': 'query',
+              'from_station_name': from_station_name,
+              'to_station_name': to_station_name,
+              'train_code': train_code,
+              'train_date': train_date
+            },
+            beforeSend: function () {
+            },
+            success: function (response) {
+              $('#result-panel').fadeIn();
+              $('#result-panel-body').append('<p>出 发：' + response.start_datetime + '</p>');
+              $('#result-panel-body').append('<p>到 达：' + response.arrive_datetime + '</p>');
+              $('#result-panel-body').append('<p>历 时：' + response.last_time + '</p>');
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+              alert(XMLHttpRequest.status);
+              alert(XMLHttpRequest.readyState);
+              alert(textStatus);
+            }
+          })
+        });
+      });
+    </script>
   </body>
 </html>
