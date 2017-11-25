e18b0f46f4489aecb68df0e88e96bddcf97acb6d
Wang Jian <337034664@qq.com>
2017-11-26 01:17:11 +0800

    gbt2260上线

--- projects/netease/gbt2260/index.php
+++ projects/netease/gbt2260/index.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 
 <head>
-  <title>POI 管家 - v1.1.1</title>
+  <title>地域助手 - v1.0</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -58,7 +58,7 @@
       <div class="col card">
         <div class="content">
           <h1 class="text-center">
-            <img class="align-top" src="../images/logo.png" height="48px" style="margin-right: 16px">POI 管家
+            <img class="align-top" src="../images/logo.png" height="48px" style="margin-right: 16px">地域助手
           </h1>
           <div class="row">
             <div class="col-sm">
@@ -69,18 +69,20 @@
               <button class="btn btn-block btn-primary" id="btn-run" type="button">开始优化</button>
               <a class="btn btn-block btn-success" id="btn-download" download style="display: none">优化完成，点击下载</a>
             </div>
-            <div class="col-sm">
+            <!-- <div class="col-sm">
               <a class="btn btn-primary btn-block" href="template.csv" download>下载模板</a>
-            </div>
+            </div> -->
           </div>
           <p class="desc">
             <b>使用说明：</b>选择表格后点击「开始优化」，稍等片刻即可下载处理后的文件。
             <br>
-            <b>字段说明：</b>每行依次为：关键词，POI1，POI2，POI3 ...
+            <b>字段说明：</b>只有一列，为源名称。
             <br>
-            <b>格式说明：</b>使用 Excel 另存为 <code>CSV UTF-8 (逗号分隔)</code> 格式。
+            <b>格式说明（重要）：</b>使用 Excel 另存为 <code>CSV UTF-8 (逗号分隔)</code> 格式。
           </p>
-          <table class="table table-sm table-hover table-bordered text-center">
+        </div>
+
+        <!-- <table class="table table-sm table-hover table-bordered text-center">
             <thead>
               <tr>
                 <th>时间</th>
@@ -89,45 +91,8 @@
               </tr>
             </thead>
             <tbody>
-<?php
-$dir = 'result_file';
-$file_list = scandir($dir, 1);
-
-foreach ($file_list as &$file_name) {
-    if ($file_name != '.' && $file_name != '..' && $file_name != '.DS_Store') {
-        $file_path = "result_file/$file_name";
-
-        $str_datetime = substr($file_name, 1, 14);
-        $datetime = date('Y-m-d H:i:s', strtotime($str_datetime));
-
-        $showname = substr($file_name, 16);
-        $filename = substr($file_name, 1);
-
-        echo <<<TBODY
-        <tr>
-            <td>$datetime</td>
-            <td><a href="original_file/O$filename" download>$showname</a></td>
-            <td><a href="result_file/R$filename" download>下载</a></td>
-        </tr>
-TBODY;
-    }
-}
-unset($file_name);
-?>
             </tbody>
-          </table>
-          <table class="table table-sm table-hover table-bordered" id="table-rule">
-            <thead>
-              <tr>
-                <th width="50%">正则</th>
-                <th>替换</th>
-              </tr>
-            </thead>
-            <tbody id="tbody-rule">
-    
-            </tbody>
-          </table>
-        </div>
+          </table> -->
 
         <form name="form" enctype="multipart/form-data">
           <input name="MAX_FILE_SIZE" type="hidden" value="20000000">
@@ -144,15 +109,15 @@ unset($file_name);
   <script src="/node_modules/bootstrap-v4/assets/js/vendor/popper.min.js"></script>
   <script src="/node_modules/bootstrap-v4/dist/js/bootstrap.min.js"></script>
   <script>
-    $(function () {
-      $.ajax({
-        url: 'rule',
-        dataType: 'json',
-        success: function (resp) {
-          parseJSON(resp);
-        }
-      })
-    })
+    // $(function () {
+    //   $.ajax({
+    //     url: 'rule',
+    //     dataType: 'json',
+    //     success: function (resp) {
+    //       parseJSON(resp);
+    //     }
+    //   })
+    // })
 
 
 
@@ -188,7 +153,7 @@ unset($file_name);
             $('#btn-import, #btn-run').remove();
             $('#btn-download').attr('href', resp.result_link);
             $('#btn-restart, #btn-download').show();
-            prependRow('tbody', resp.datetime, resp.original_link, resp.file_name, resp.result_link);
+            // prependRow('tbody', resp.datetime, resp.original_link, resp.file_name, resp.result_link);
             $('#tr-new').fadeIn('slow');
             alert('优化完成！');
           }
