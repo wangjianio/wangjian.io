8ea4db691ceb9f67cffe90514a3203b9d53b6a98
Wang Jian <337034664@qq.com>
2017-12-02 21:14:04 +0800

    新增 blog api

---
+++ api/blog/post.php
@@ -0,0 +1,19 @@
+<?php
+$file_list = scandir('posts');
+
+foreach ($file_list as $file_name) {
+    if (preg_match("/\d{6}\.\d+\.\d\.\d\..+/", $file_name)) {
+        $post_info = explode('.', $file_name);
+
+        $arr['pubDate']     = date('Y-m-d', strtotime($post_info[0]));
+        $arr['id']          = $post_info[1];
+        $arr['valid']       = $post_info[2];
+        $arr['showInIndex'] = $post_info[3];
+        $arr['title']       = $post_info[4];
+        $arr['editDate']    = date('Y-m-d', filemtime("posts/$file_name"));
+
+        $json[] = $arr;
+    }
+}
+
+echo json_encode($json);
