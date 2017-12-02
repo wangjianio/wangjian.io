5b5f98e90717d5154496af94af228cb1ddba0e98
Wang Jian <337034664@qq.com>
2017-12-02 21:50:52 +0800

    更新 blog api

--- api/blog/post.php
+++ api/blog/post.php
@@ -4,14 +4,15 @@ $file_list = scandir('posts');
 foreach ($file_list as $file_name) {
     if (preg_match("/\d{6}\.\d+\.\d\.\d\..+/", $file_name)) {
         $post_info = explode('.', $file_name);
-
-        if ($post_info[2] && $post_info[3]) {
-        $arr['pubDate']     = date('Y-m-d', strtotime($post_info[0]));
-        $arr['id']          = $post_info[1];
-        // $arr['valid']       = $post_info[2];
+        
         // $arr['showInIndex'] = $post_info[3];
-        $arr['title']       = $post_info[4];
-        $arr['editDate']    = date('Y-m-d', filemtime("posts/$file_name"));
+        // $arr['valid']       = $post_info[2];
+        // $arr['editDate'] = date('Y-m-d', filemtime("posts/$file_name"));
+        if ($post_info[2] && $post_info[3]) {
+
+            $arr['id']       = $post_info[1];
+            $arr['title']    = $post_info[4];
+            $arr['pubDate']  = date('Y-m-d', strtotime($post_info[0]));
 
             $json[] = $arr;
         }
