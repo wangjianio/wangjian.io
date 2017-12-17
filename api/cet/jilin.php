155e51866eb1edd5d085b041bcd68c6d759160ee
Wang Jian <337034664@qq.com>
2017-12-17 22:13:04 +0800

    cet jilin api

---
+++ api/cet/jilin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * method: post
+ * 只接受 15 或 18 位的 idNumber
+ */
+// if (!$_POST['idNumber']) exit;
+
+$id = $_GET['idNumber'];
+
+switch (strlen($id)) {
+    case 15:
+    case 18:
+        if (!preg_match("/(^\d{17}(\d|X|x)$)|(^\d{15}$)/", $id)) {
+            $arr['error'] = '身份证号格式有误';
+        } else {
+            $page = file_get_contents("http://cet.jlste.com.cn/cet/query/qry/$id");
+
+            preg_match('/<td>考生姓名<\/td>\s<td>(.*)<\/td>/', $page, $name);
+            preg_match('/<td>身份证号<\/td>\s<td>(.*)<\/td>/', $page, $id_no);
+            preg_match('/<td>准考证号<\/td>\s<td>(.*)<\/td>/', $page, $cet_no);
+
+            if (empty($cet_no)) {
+                $arr['error'] = '没有查到信息';
+            } else {
+                $arr['data']['name'] = $name[1];
+                $arr['data']['idNumber'] = $id_no[1];
+                $arr['data']['certificate'] = $cet_no[1];
+            }
+        }
+        break;
+
+    default:
+        $arr['error'] = '身份证号应为 15 或 18 位';        
+        break;
+}
+
+$arr['error'] ? $arr['result'] = false : $arr['result'] = true;
+
+echo json_encode($arr);
