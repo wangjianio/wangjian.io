0a325b447dae95b60078a15771bd77369003a5e3
王建 <wangjianio@users.noreply.github.com>
2020-11-23 00:04:40 +0800

    clear v2

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './assets/primer-markdown-formated.css';
-import registerServiceWorker from './utils/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
